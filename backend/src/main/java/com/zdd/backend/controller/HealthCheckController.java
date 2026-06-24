ba537b5a6271c8bbf49f4404bed2969b4867ffa5
tripthelight <bye_ok@naver.com>
2026-06-24 21:29:30 +0900

    [260624] - first commit

---
+++ backend/src/main/java/com/zdd/backend/controller/HealthCheckController.java
@@ -0,0 +1,39 @@
+package com.zdd.backend.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("serverPort", serverPort);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverName", serverName);
+        responseData.put("env", env);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+
+}
