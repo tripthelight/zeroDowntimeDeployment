d9c8ee0575b7facc3ce9f93ef20e3084323b9820
tripthelight <bye_ok@naver.com>
2026-06-28 22:05:51 +0900

    [260628] - first commit

---
+++ src/main/java/com/zdd/zerodowntimedeployment/ZeroDowntimeDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.zdd.zerodowntimedeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDowntimeDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroDowntimeDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdd/zerodowntimedeployment/ZeroDowntimeDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdd.zerodowntimedeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDowntimeDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
