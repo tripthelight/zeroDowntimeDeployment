8e00357cdd6d92fdd2d6a5197cb7ffd3ff95a6dd
tripthelight <bye_ok@naver.com>
2026-06-29 02:31:18 +0900

    [260629] - zdd test green

--- src/main/java/com/zdd/zerodowntimedeployment/controller/HealthCheckController.java
+++ src/main/java/com/zdd/zerodowntimedeployment/controller/HealthCheckController.java
@@ -27,8 +27,6 @@ public class HealthCheckController {
         responseData.put("serverName", serverName);
         responseData.put("env", env);
 
-        System.out.println("serverPort" + serverPort);
-
         return ResponseEntity.ok(responseData);
     }
 
